8aab3db36092aa0addeb052f50bb27bb94c44f0a
migyoeng <wjdalrud5516@gmail.com>
2024-11-22 11:12:52 +0900

    Main Controller 만들었어요 이거 사용하시면 됩니다

---
+++ demo/src/main/java/com/example/demo/main_controller.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class main_controller {
+
+	@GetMapping("/index.do")
+	public String index() {
+		return null;
+	}
+}
